9411f9ea1c0b9318559dba21fea1ae1321e20bad
padrisimo <chamizho@gmail.com>
2020-03-12 17:21:51 +0100

    flipCard method

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
     }
     
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+    
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
 
+
+    }
 }
 
